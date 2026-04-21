75da5c718cf19325c4c528d1d55994f9f2317db7
Ura_Myhal <uramigal8@gmail.com>
2026-04-21 20:50:48 +0300

    Доповнення і повернення index

--- check_index.sh
+++ check_index.sh
@@ -2,10 +2,8 @@
 
 if [ -f "index.html" ]; then
   echo "index.html exists"
-  echo "FOUND=true" >> $GITHUB_ENV
   exit 0
 else
   echo "index.html does not exist"
-  echo "FOUND=false" >> $GITHUB_ENV
   exit 1
 fi
